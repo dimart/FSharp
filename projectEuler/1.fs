3115c069f548372c565749cf49b1042a69583b49
Дмитрий Петухов <dimart.sp@gmail.com>
2013-03-25 14:34:18 +0400

    Problem#1

--- projectEuler/1.fs
+++ projectEuler/1.fs
@@ -1,6 +1,4 @@
-/* Problem #1
- * Find the sum of all the multiples of 3 or 5 below 1000 
- * http://projecteuler.net/problem=1
- */
+// Problem #1
+// Find the sum of all the multiples of 3 or 5 below 1000
  
 Seq.sum <| seq { for x in 0..999 -> if (x % 3 = 0) || (x % 5 = 0) then x else 0 } 
