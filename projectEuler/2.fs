76e2c37bd37774b3e7cf3a45f0d8c272ada5c93a
Дмитрий Петухов <dimart.sp@gmail.com>
2013-04-01 03:33:20 +0400

    Problem #2

--- projectEuler/2.fs
+++ projectEuler/2.fs
@@ -1,5 +1,5 @@
 // Problem 2
-// By considering the terms in the Fibonacci sequence whose values do not exceed four million,
+// By considering the terms in the Fibonacci sequence whose values do not exceed four million
 // find the sum of the even-valued terms
 
 open System
