b9cc931edd5adc00dad5d97fef7f4662ca6e5afd
Dmitry <dimart.sp@gmail.com>
2013-05-23 21:56:54 +0400

    10.fs changed

--- projectEuler/10.fs
+++ projectEuler/10.fs
@@ -1,12 +1,11 @@
 // Problem 10
 // Find the sum of all the primes below two million.
 
-let isPrime x = 
-    let mutable i = 2
-    while x % i <> 0 do
-        i <- i + 1
-        if (i > int (sqrt (float x))) then i <- x
-    i = x
-
-let ans = {2..2000000} |> Seq.filter isPrime |> Seq.map int64 |> Seq.sum
+let isPrime n =
+    if n <= 1L then false
+    else
+        let m = int64 (sqrt (float(n)))
+        Seq.forall (fun i -> (n % i) <> 0L) {2L..m}
+        
+let ans = {2L..2000000L} |> Seq.filter isPrime |> Seq.sum
 printfn "%A" ans
