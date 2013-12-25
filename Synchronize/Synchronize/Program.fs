a8843f23c3688e50b9d929c97c7363796c961078
dimart <dimart.sp@gmail.com>
2013-12-25 19:44:56 +0400

    sketch updated

--- Synchronize/Synchronize/Program.fs
+++ Synchronize/Synchronize/Program.fs
@@ -1,17 +1,24 @@
 ﻿open System
 
 let processes = 5
+let rnd       = new Random()
+let mutable q = [| for i in 0..processes -> false |]
 
 let asyncProcess(i : int) = 
+    let time = rnd.Next(1,100)
+    printfn "Process №%A will wait %A" i time
     async {
-        do! Async.Sleep((new Random()).Next(1,1000))
-        printfn "Process waited %A ms" (i+1)
+        do! Async.Sleep(time)
+        printfn "Process №%A completed!" i   
     }
 
-let asyncProcesses = [for i in 1 .. processes -> asyncProcess(i)]
+let asyncProcesses = [for i in 1 .. processes -> asyncProcess(i)]     
 
 [<EntryPoint>]
 let main _ =
+    printfn "------------------------"
     asyncProcesses |> Async.Parallel |> Async.RunSynchronously |> ignore
+    printfn "------------------------"
+    printfn "Done!"
     Console.ReadKey() |> ignore
     0
