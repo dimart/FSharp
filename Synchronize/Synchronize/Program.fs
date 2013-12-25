7b8a5ca03cc2dd92690cecdf07c5e1b4659727e7
dimart <dimart.sp@gmail.com>
2013-12-25 19:58:40 +0400

    sketch updated

--- Synchronize/Synchronize/Program.fs
+++ Synchronize/Synchronize/Program.fs
@@ -2,14 +2,19 @@
 
 let processes = 5
 let rnd       = new Random()
-let mutable q = [| for i in 0..processes -> false |]
+let mutable q = [|for i in 0 .. (processes - 1) -> false|]
+
+let printProcess(i : int) =
+    lock q ( fun _ -> q.[i-1] <- true
+                      printfn "It's process №%A" i
+           )
 
 let asyncProcess(i : int) = 
     let time = rnd.Next(1,100)
     printfn "Process №%A will wait %A" i time
     async {
         do! Async.Sleep(time)
-        printfn "Process №%A completed!" i   
+        printProcess i   
     }
 
 let asyncProcesses = [for i in 1 .. processes -> asyncProcess(i)]     
