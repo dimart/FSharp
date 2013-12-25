e777cb6d31bfcaef1598ef49d175fe488c2de040
dimart <dimart.sp@gmail.com>
2013-12-25 22:30:11 +0400

    Edited: now test shows where was an error; and parseEmail function was modified

--- EmailCheck/EmailCheck/Program.fs
+++ EmailCheck/EmailCheck/Program.fs
@@ -10,15 +10,15 @@ type EmailCheck() =
         let domain  = "(com|edu|info|museum|name|net|org|[a-z][a-z])"
         let pattern = "^" + username + "@" + hostname + domain + "$"
 
-        member x.parseEmail (email : string) = Regex.IsMatch(email, pattern)
+        member x.parseEmail (email : string) = Regex.IsMatch(email.ToLower(), pattern)
     end
                          
 [<TestFixture>]
 type EmailTests() =
     let emailCheck = new EmailCheck()
 
-    let corrects   = [ "ab5cd@gmail.com";
-                       "abc3d.abcd@gmail.com";
+    let corrects   = [ "Ab5cd@gmail.com";
+                       "Abc3d.abcd@gmail.com";
                        "!abcd2@gmail.com";
                        "-abcd4@gmail.ru";
                        "abcd.abcd.abcd@gmail.com";
@@ -26,7 +26,7 @@ type EmailTests() =
                        "abcd@g.com";
                        "abcd@my.site.the.best.com" ]
 
-    let incorrects = [ "abcd@-gmail.com";
+    let incorrects = [ "Abcd@-gmail.com";
                        "abcd..@gmail.com";
                        "abcd@.gmail.n";
                        ".abcd@gmail.nettt";
@@ -35,8 +35,12 @@ type EmailTests() =
                        "abcdgmail.com";
                        "abcd@my.site.the.worse..com" ]
     [<Test>]
-    member x.Corrects()   = corrects |> List.iter (fun e -> emailCheck.parseEmail(e) |> should be True)
+    member x.Corrects()   = corrects |> List.iter (fun e -> let t = emailCheck.parseEmail(e) in 
+                                                            Assert.AreEqual(true, t, "Error in email:" + e)
+                                                            )
 
     [<Test>]
-    member x.Incorrects() = incorrects |> List.iter (fun e -> emailCheck.parseEmail(e) |> should be False)
+    member x.Incorrects() = incorrects |> List.iter (fun e -> let t = emailCheck.parseEmail(e) in 
+                                                              Assert.AreEqual(false, t, "Error in email:" + e)
+                                                              )
     
